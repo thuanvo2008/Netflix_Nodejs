535fb7870d8feea4aff9c50b6c89f8b726f17932
thuanvo2008 <thuanvip322@gmail.com>
2022-03-31 01:57:04 +0700

    config DB

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv/config')
 
-main().catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_DB,{ useNewUrlParser: true })
+.then(console.log('connecting DB'))
+.catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect(process.env.MONGODB_)
-  .then(console.log('connecting DB')
-  .catch((err) => console.log(err)));
-}
 
 const port = 3000;
 app.listen(port, () => {
